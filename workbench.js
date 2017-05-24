09d572254edb08aa162861f93b596ea877e774e5
Josh Gibbs <uPaymeiFixit@gmail.com>
2017-05-24 01:41:30 -0700

    Refactored Prim's algorithm

--- workbench.js
+++ workbench.js
@@ -1,38 +1,41 @@
 require('./utils.js');
 MODE = DEG;
 
-function Prim (/*Cost, n, T, MinCost*/) {
+const OB = IN = 9999; // Out of bounds & Infinity
+
+function Prim (Cost) {
+    const n = Cost.length - 1;
+    const T = [];
+
     let MinCost = 0;
-    Near[1] = 0;
-    for (let i = 2; i < Cost.length; i++) {
+    const Near = [OB, 0];
+    for (let i = 2; i <= n; i++) {
         Near[i] = 1;
     }
-    for (let i = 1; i < Cost.length - 1; i++) {
-        let j = minJ();
+    for (let i = 1; i <= n - 1; i++) {
+        // let j be an index s.t. (Near(j) != 0) and Cost(j, Near(j)) is minimum;
+        let min_j;
+        let min_cost = IN;
+        for (let j = 1; j <= n; j++) {
+            if (Near[j] != 0 && Cost[j][Near[j]] < min_cost) {
+                min_j = j;
+                min_cost = Cost[j][Near[j]];
+            }
+        }
+        let j = min_j;
+
         T.push(`(${j}, ${Near[j]})`); // add edge to the Minimum Spanning Tree
         MinCost += Cost[j][Near[j]];
         Near[j] = 0;
-        for (let k = 1; k < Cost.length; k++) {
+        for (let k = 1; k <= n; k++) {
             if (Near[k] != 0 && Cost[k][Near[k]] > Cost[k][j]) {
                 Near[k] = j; // update cost if lower
             }
         }
     }
-    return MinCost;
+    return {MinCost: MinCost, T: T};
 }
 
-function minJ () {
-    let min_j = 1;
-    let min_cost = Cost[1][Near[1]];
-    for (let j = 2; j < Cost.length; j++) {
-        if (Near[j] != 0 && Cost[j][Near[j]] < min_cost) {
-            min_j = j;
-            min_cost = Cost[j][Near[j]];
-        }
-    }
-    return min_j;
-}
-const OB = IN = 9999;
 const Cost = [
 /*      OB   1   2   3   4   5   6 */
 /*OB*/ [OB, OB, OB, OB, OB, OB, OB],
@@ -43,9 +46,7 @@ const Cost = [
 /* 5*/ [OB, 19, IN, IN, 18,  0, 33],
 /* 6*/ [OB, 21, 11, IN, 14, 33,  0]
 ];
-const Near = [];
-Near[0] = OB;
-const T = [];
 
-out.red(Prim());
-T.print();
+const o = Prim(Cost);
+out.red(o.MinCost);
+o.T.print();
